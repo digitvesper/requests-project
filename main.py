b988065a5b95a933b179fd2e87eb176391cfd3f4
digitvesper <digitvesper@gmail.com>
2024-06-06 15:00:55 +0300

    st

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+import pprint
+
+params = {
+    'q' : 'python'
+}
+response = requests.get("https://api.github.com/search/repositories", params=params)
+response_json = response.json()
+pprint.pprint(response_json)
